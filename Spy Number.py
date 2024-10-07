df3f7f1a10756982847b0669a4ad2acbd9e95915
S.Jahnavi <147792372+23P31A0555@users.noreply.github.com>
2024-10-07 15:07:24 +0530

    Uploading code: Spy Number.py

---
+++ Spy Number.py
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Spy{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,sum=0,pro=1,r;
+        n=sc.nextInt();
+        while(n!=0)
+        {
+            r=n%10;
+            sum=sum+r;
+            pro=pro*r;
+            n=n/10;
+        }
+        if(sum==pro)
+        {
+            System.out.println("Spy Number");
+        }
+        else
+        {
+            System.out.println("Not Spy Number");
+        }
+    }
+}
