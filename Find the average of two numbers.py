b1bdbe3b1a086321199ca0b8c6ce95bd2c28c4a4
S.Jahnavi <147792372+23P31A0555@users.noreply.github.com>
2024-10-11 20:10:22 +0530

    Uploading code: Find the average of two numbers.py

---
+++ Find the average of two numbers.py
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Average{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        double a,b;
+        a=sc.nextDouble();
+        b=sc.nextDouble();
+        double c=(a+b)/2;
+        System.out.printf("%.4f",c);
+    }
+}
